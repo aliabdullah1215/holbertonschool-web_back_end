26a319c884d3f386f5b82592cea24a5ab6eefe38
root <root@ip-10-42-53-142.ec2.internal>
2026-02-26 21:40:11 +0000

    task 1: async comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that defines an asynchronous comprehension to collect random numbers.
+"""
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers from async_generator using async comprehension.
+    """
+    return [value async for value in async_generator()]
