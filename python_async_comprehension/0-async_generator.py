67ce262ece6d3ea535bcc3ae9850aa1bda9aa299
root <root@ip-10-42-53-142.ec2.internal>
2026-02-26 21:36:56 +0000

    task 0: async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that defines an asynchronous generator yielding random numbers.
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Asynchronously yields 10 random float values between 0 and 10, one per second.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
