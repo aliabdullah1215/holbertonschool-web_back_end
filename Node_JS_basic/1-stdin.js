439e2be24a3ad17de3e0e9474bc3999b753a15fc
root <root@ip-10-42-137-169.ec2.internal>
2026-04-08 15:57:53 +0000

    Fix stdin handling

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,23 +2,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-let inputData = '';
-
 process.stdin.on('data', (chunk) => {
-  inputData += chunk;
+  process.stdout.write(`Your name is: ${chunk}`);
 });
 
 process.stdin.on('end', () => {
-  const name = inputData.trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  }
   process.stdout.write('This important software is now closing\n');
 });
-
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (chunk) => {
-    const name = chunk.trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-  });
-}
